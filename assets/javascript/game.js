1988dd1adcecec80a994d435e487aa3599d598e3
Paula MacTavish <perilousp@hotmail.com>
2018-03-01 23:39:45 -0500

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,93 +1,125 @@
-function show(elementId){
-    document.getElementById("splashScreen").style.display="none";
-    document.getElementById("hangmanGame").style.display="none";
-    document.getElementById(elementId).style.display="block";
+function show(elementId) {
+  document.getElementById("splashScreen").style.display="none";
+  document.getElementById("hangmanGame").style.display="none";
+  document.getElementById("loseScreen").style.display="none";
+  document.getElementById("winScreen").style.display="none";
+  document.getElementById(elementId).style.display="block";
+}  
+// variables - global
+var winsCounter = (0);
+var lossesCounter = (0);
+var guessesLeft = 10;
+var wordBank = ["sunshine", "thunder", "lightening", "hurricane", "tornado", "tsunami", "sleet", "hail", "snow", "rain"];
+var randWord = ""
+var chosenWord = [];
+var underScore = [];
+var guess;
+var wrongLetter = [];
+var playing = true
+
+// start Game parameters
+function startGame() { 
+  console.log(chosenWord);
+  console.log(underScore)
+  console.log(guessesLeft);
+  document.getElementById("winsCounter").innerHTML = winsCounter;
+  document.getElementById("lossesCounter").innerHTML = lossesCounter;
+  document.getElementById("guessesLeft").innerHTML = guessesLeft;
+  document.getElementById("notLetter").innerHTML = "Press a Letter to Begin"
 }
 
-// Hangman word options //
-var wordBank = ["sunshine", "thunder", "lightening", "hurricane", "tornado", "tsunami", "sleet", "hail", "snow", "rain"];
-// variables
-var guess;
-var rightLetter = [];
-var wrongLetter = [];
-var guessesLeft = 10;
-var wins = 0;
-var losses = 0;
-var underScore = [];
-var docUnderScore = document.getElementsByClassName('underScore');
-var docRightLetter = document.getElementsByClassName('rightLetter');
-var docWrongLetter = document.getElementsByClassName('wrongLetter');
-
-function startGame(){
-    
 // choses word from word option array //
 randWord = Math.floor(Math.random() * wordBank.length);
+
 // renames randomly chosen word from number in array to chosenWord
-var chosenWord = wordBank[randWord];
-console.log(chosenWord);
-// create underscores based on word length //
-var generateUnderscore = () => {
-    for(var i = 0; i < chosenWord.length; i++) {
-        underScore.push("_");
+chosenWord = wordBank[randWord];
+
+// creates a loop to run = to the length of the array
+for(var i = 0; i < chosenWord.length; i++) { 
+
+  // pushes _ into the global underScore array each time the for loop runs
+  underScore.push("_");
+    
+  //print underscores to screen remove comma by reintigrating letters into word (.join) 
+  document.getElementById("underScore").innerHTML = underScore.join(" "); 
+  }  
+
+// Main Game Play 
+// Capture keyboard input - user guesses
+document.onkeyup = function(keyPressed) {
+
+  // checks if key pressed has a value of 65 to 90 - the values of A to Z in javascript
+  
+  if (keyPressed.which <= 65 && keyPressed.which < 91) {
+    // Error message if a letter is not pressed
+    document.getElementById('notLetter').style.color = "rgba(194, 19, 19, 0.824)";
+    document.getElementById("notLetter").innerHTML = "Please Press a Letter" + "<br />" + wrongLetter.join(" ");
     }
-// print underscores to screen remove comma by reintigrating letters into word (.join) 
-docUnderScore[0].innerHTML = underScore.join(" ");
-return underScore;
-} 
-console.log(generateUnderscore());
-// print guesses remaining, wins and losses to screen
-document.getElementById("guessesLeft").innerHTML = guessesLeft;
-document.getElementById("wins").innerHTML = wins;
-document.getElementById("losses").innerHTML = losses;
-// Main Game - Capture keyboard input - user guesses
-document.onkeyup = function(event) {
-    //  Converts letter input to lowercase, and saves it to a variable guess
-var guess = String.fromCharCode(event.which).toLowerCase();
-console.log (guess);
-    // see if guess is right 
+  
+  if (keyPressed.which >= 65 && keyPressed.which <=90) {
+  
+    // erases error message when a letter is pressed and returns screen to just wrong guesses
+    document.getElementById('notLetter').style.color = "#0c0d57";
+    document.getElementById("notLetter").innerHTML = wrongLetter.join(" ")
+    
+    // sets a global value to guess
+    keyPressed = guess;
+
+    // Converts letter input unicode value to string, it uppercase letter, and saves it to a variable guess
+    guess = String.fromCharCode(event.which).toLowerCase();
+
+    // check if guessed letter is in word
     if(chosenWord.indexOf(guess) > -1) {
-        // loop through entire word for multiple instances of the same letter    
-        for (i = 0; i < chosenWord.length; i++){
-            // replace underscore with letter then print to screen
-            if(chosenWord[i] === guess) {
-                underScore[i] = guess;
-                rightLetter.push(guess);
-                underScore[chosenWord.indexOf(guess)] = guess;
-                docUnderScore[0].innerHTML = underScore.join(" ");
-            }    
-        } 
-    }    
-    else {
-        wrongLetter.push(guess);
-        docWrongLetter[0].innerHTML = wrongLetter.join(" ");
-        guessesLeft--; 
-        if (guessesLeft < 1) {
-            losses++;
-            document.getElementById("hangmanGame").style.display="none";
-            document.getElementById("loseScreen").style.display="block";
-        }
+
+      // loop through entire array for multiple instances of the same letter    
+      for (i = 0; i < chosenWord.length; i++){
+
+        // replace underscore with letter then print to screen
+        if(chosenWord[i] == guess) {
+          underScore[i] = guess;
+          underScore[chosenWord.indexOf(guess)] = guess;
+          document.getElementById("underScore").innerHTML = underScore.join(" ");
+        }  
+      }
     } 
 
-    // joins all the correctly chosen letters into a word and checks to see if all the letters in the word have been guessed
-    if(underScore.join("") == chosenWord) {
-        wins++;
-        document.getElementById("hangmanGame").style.display="none";
-        document.getElementById("winScreen").style.display="block";
-    }
-    }  
-}     
-startGame();  
-
- // Reset
-
- document.getElementById('reset').onclick = function() {
-    randWord = [];
+  // joins all the correctly chosen letters into a word and checks to see if all the letters in the word have been guessed
+  if (underScore.join("") === chosenWord) {
+    winsCounter++;
+    randWord = ""
     chosenWord = [];
-    underScore = []; 
-    wrongLetter = [0];
-    guessesLeft = 10;
-    document.getElementById("winScreen").style.display="none";
-    document.getElementById("loseScreen").style.display="none";
-    document.getElementById("hangmanGame").style.display="block";
-    startGame();
-}
+    underScore = [];
+    guess;
+    wrongLetter = [];
+    guessesLeft = 10
+    document.getElementById("hangmanGame").style.display="none";
+    document.getElementById("winScreen").style.display="block";
+  }
+  
+  // guess was wrong and you still have lives remaing
+  if(chosenWord.indexOf(guess) <= -1) {
+    guessesLeft--;
+    wrongLetter.push(guess);
+    document.getElementById("notLetter").innerHTML = wrongLetter.join(" ")
+    document.getElementById("guessesLeft").innerHTML = guessesLeft;
+  } 
+  
+  // if no guesses left
+  if (guessesLeft < 1) {
+    lossesCounter++;
+    document.getElementById("lossesCounter").innerHTML = lossesCounter;
+    document.getElementById("correct").innerHTML = chosenWord;
+    randWord = ""
+    chosenWord = [];
+    underScore = [];
+    guess;
+    wrongLetter = [];
+    guessesLeft = 10
+    // document.getElementById("notLetter").innerHTML = wrongLetter
+    document.getElementById("hangmanGame").style.display="none";
+    document.getElementById("loseScreen").style.display="block";
+  } 
+}       
+                 
+}              
+startGame();
